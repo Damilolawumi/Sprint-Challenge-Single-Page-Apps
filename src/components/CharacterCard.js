f1351a402e9f667166353359d048cb2e7898fa9e
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-09-20 11:00:59 +0100

    added styled component

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,13 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
+const StyledDiv = styled.div`
+margin: 20px;
+width:250px;
+box-shadow: 2px 2px 4px 2px;
+`;
 export default function CharacterCard(props) {
   return (
-   <div>
+   <StyledDiv>
     <h2>{props.character.name}</h2>
     <h3>{props.character.specie}</h3>
     <h4>{props.character.gender}</h4>
-  </div>
+  </StyledDiv>
   )
   
 }
