a4b05e80f5712d95bacf52f511f6c000ea533184
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-09-20 09:45:29 +0100

    displayed 3 elements for each character

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Route } from 'react-router-dom';
 import Header from "./components/Header.js";
+
+import CharacterList from "./components/CharacterList";
 
 
 export default function App() {
+
+
   return (
     <main>
       <Header />
+      <CharacterList />
+<Route/>
+<Route/>
+<Route/>
+<Route/>
+
+
     </main>
   );
 }
